ac6818f13ead256c8dc00d79e3427cb4931ca9c7
Manuel-Ko <Manuel-Ko@users,noreply.github.com>
2014-06-08 23:11:28 +0200

    Swirl working (bad interaction)

--- sheet07_task18_Swirl/Swirl.cu
+++ sheet07_task18_Swirl/Swirl.cu
@@ -22,25 +22,28 @@ float *swirlDevPtr;				// device memory for swirled image
 const int PIC_SIZE = DIM * DIM;
 const int PIC_BYTE_SIZE = PIC_SIZE * sizeof(float);
 
-__device__ void calcRotatedPos(float alpha, float2 inPos, float2& calcPos)
+float a = 0;
+float b = 0;
+
+__device__ void calcRotatedPos(float alpha, float2 inPos, int2& calcPos)
 {
 	inPos.x -= centerX;
 	inPos.y -= centerY;
 	float cosA = cos(alpha);
 	float sinA = sin(alpha);
 	float resx = cosA * inPos.x - sinA * inPos.y;
-	float resy = sinA * inPos.x - cosA * inPos.y;
+	float resy = sinA * inPos.x + cosA * inPos.y;
 	//calcPos.x = cosA * inPos.x - sinA * inPos.y;
 	//calcPos.y = sinA * inPos.x - cosA * inPos.y;
 	resx += centerX;
 	resy += centerY;
-	calcPos.x = resx;
-	calcPos.y = resy;
+	calcPos.x = roundf(resx);
+	calcPos.y = roundf(resy);
 }
 
 __global__ void swirlKernel( float *sourcePtr, float *targetPtr, float a, float b ) 
 {
-	int index = 0;
+	int index1 = 0;
     // TODO: Index berechnen	
 	/*int pixelX = blockIdx.x * blockDim.x + threadIdx.x;
 	int pixelY = blockIdx.y * blockDim.y + threadIdx.y;
@@ -49,7 +52,7 @@ __global__ void swirlKernel( float *sourcePtr, float *targetPtr, float a, float 
 	// von Cord
 	int blockSz = blockDim.x * blockDim.y;
 	int i = threadIdx.x + threadIdx.y * blockDim.x + blockSz * (blockIdx.x + blockIdx.y * gridDim.x); 
-	index = i;
+	index1 = i;
 
 	
 	float2 pix;
@@ -69,12 +72,70 @@ __global__ void swirlKernel( float *sourcePtr, float *targetPtr, float a, float 
 	float alpha = a * pow(r,b);
 
 	// TODO: Den swirl invertieren.
-	float2 grabPos;
+	int2 grabPos;
 	calcRotatedPos(alpha, pix, grabPos);
 
-	index = grabPos.x + grabPos.y * DIM;
+	if(grabPos.x >= DIM || grabPos.x < 0)
+	{
+		grabPos.x = pix.x;
+		grabPos.y = pix.y;
+	}
+	else if(grabPos.y >= DIM || grabPos.y < 0)
+	{
+		grabPos.x = pix.x;
+		grabPos.y = pix.y;
+	}
+	int index2 = 0;
+	index2 = grabPos.x + grabPos.y * DIM;
 
-	targetPtr[index] = sourcePtr[index];    // simple copy
+	targetPtr[index1] = sourcePtr[index2];    
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	const float aStep = 0.01;
+	const float bStep = 0.005;
+	switch (key)
+	{
+		case 27:
+			exit(0);	
+			break;
+		case '1': 
+			a -= aStep; 
+			a = std::max(a,-2.0f);
+
+			std::cout << "a: " << a << std::endl;
+			break;
+		case '2': 
+			a += aStep;
+			a = std::min(a, 2.0f);
+
+			std::cout << "a: " << a << std::endl;
+			break;
+
+		case '3': 
+			b -= bStep;
+			b = std::max(b, 0.0f);
+
+			std::cout << "b: " << b << std::endl;
+			break;
+		case '4': 
+			b += bStep;
+			b = std::min(b, 1.0f);
+
+			std::cout << "b: " << b << std::endl;
+			break;
+		case '+':
+			
+			break;
+		case '-':
+			
+			break;
+		case 'w':
+			
+			break;
+	}
+	glutPostRedisplay();
 }
 
 void display(void)	
@@ -85,7 +146,7 @@ void display(void)
 	// TODO: Swirl Kernel aufrufen.
 	dim3 gridSize(64,64);
 	dim3 blocksize(8,8);
-	swirlKernel<<<1024 ,256>>>(sourceDevPtr, swirlDevPtr, 1,0.3);
+	swirlKernel<<<1024 ,256>>>(sourceDevPtr, swirlDevPtr, a,b);
 
 	// TODO: Ergebnis zu host memory zuruecklesen.
 	cudaMemcpy(readBackPixels, swirlDevPtr,PIC_BYTE_SIZE, cudaMemcpyDeviceToHost);
@@ -110,6 +171,7 @@ int main(int argc, char **argv)
 	glutCreateWindow("Simple OpenGL CUDA");
 	glutIdleFunc(display);
 	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
 
 	// load bitmap	
 	Bitmap bmp = Bitmap("who-is-that.bmp");
