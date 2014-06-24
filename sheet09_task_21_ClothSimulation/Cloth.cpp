722e3fab234b21ed0b38440ce401b4ee09ee0455
Manuel-Ko <fergo34@yahoo.de>
2014-06-24 13:59:59 +0200

    added first try for task 21 cloth simulation

---
+++ sheet09_task_21_ClothSimulation/Cloth.cpp
@@ -0,0 +1,122 @@
+#include "Cloth.h"
+#include <GL/glew.h>
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+#include "common.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+void updateCloth(	float3* newPos, float3* oldPos, float3* impacts, float3* velocity,					
+					float deltaTime, float stepsize);
+
+ClothSim::ClothSim() : ping(0)
+{
+	vboPos[0] = 0;
+	vboPos[1] = 0;
+	unsigned int memSize = sizeof(float) * 3 * RESOLUTION_X*RESOLUTION_Y;
+	
+	// Initialize mesh
+	float ratio = RESOLUTION_Y / (float)RESOLUTION_X;
+	float* m_hPos = new float[3 * RESOLUTION_X*RESOLUTION_Y];
+	int j=0;
+	for (int x=0; x<RESOLUTION_X; ++x)
+	{
+		for (int y=0; y<RESOLUTION_Y; ++y)
+		{
+			m_hPos[j*3] = x/(float)RESOLUTION_X - 0.5f;
+			m_hPos[j*3+1] = 1;
+			m_hPos[j*3+2] = y/(float)RESOLUTION_Y * ratio - (ratio);
+			++j;
+		}
+	}
+
+	// allocate device memory for intermediate impacts and velocities.
+	CUDA_SAFE_CALL(cudaMalloc((void**)&devPtrImpact, memSize));
+	CUDA_SAFE_CALL(cudaMalloc((void**)&devPtrVelocity, memSize));
+	cudaMemset(devPtrImpact, 0, RESOLUTION_X*RESOLUTION_Y*sizeof(float3));
+	cudaMemset(devPtrVelocity, 0, RESOLUTION_X*RESOLUTION_Y*sizeof(float3));
+	
+	// TODO: Erzeugen der VBOs für die Positionen und Verbindung zu CUDA herstellen.
+	glGenBuffers(2,vboPos);
+	glBindBuffer(GL_ARRAY_BUFFER, vboPos[0]);
+	glBufferData(GL_ARRAY_BUFFER, 3*RESOLUTION_X*RESOLUTION_Y*sizeof(float),m_hPos,GL_DYNAMIC_COPY);
+	glBindBuffer(GL_ARRAY_BUFFER, vboPos[1]);
+	glBufferData(GL_ARRAY_BUFFER, 3*RESOLUTION_X*RESOLUTION_Y*sizeof(float),m_hPos,GL_DYNAMIC_COPY);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	CUDA_SAFE_CALL(cudaGraphicsGLRegisterBuffer(&cudaPos[0],vboPos[0], cudaGraphicsRegisterFlagsNone));
+	CUDA_SAFE_CALL(cudaGraphicsGLRegisterBuffer(&cudaPos[1],vboPos[1], cudaGraphicsRegisterFlagsNone));
+	// TODO VBO vboNormal erzeugen und mit cudaNormal verknüpfen. Das VBO braucht keine initialen Daten (NULL übergeben).
+	
+	delete[] m_hPos;
+}
+
+ClothSim::~ClothSim()
+{
+    CUDA_SAFE_CALL(cudaGraphicsUnregisterResource(cudaPos[0]));
+    CUDA_SAFE_CALL(cudaGraphicsUnregisterResource(cudaPos[1]));
+	// TODO cudaNormal freigeben
+    glDeleteBuffers(2, (const GLuint*)vboPos);
+	// TODO vboNormal freigeben
+	CUDA_SAFE_CALL( cudaFree( devPtrImpact ) ); 
+	CUDA_SAFE_CALL( cudaFree( devPtrVelocity ) ); 
+}
+
+void ClothSim::update(GLfloat deltaTime)
+{
+	// Lokale Variablen, in die die Pointer auf die Daten der CUDA-Ressourcen abgelegt werden können.
+	float* oldPos = NULL;
+	float* newPos = NULL;
+	float* normals = NULL;
+	
+	// TODO: Map cudaPos (Hinweis: cudaGraphicsMapResources)
+	CUDA_SAFE_CALL(cudaGraphicsMapResources(2,cudaPos));
+	// TODO: Map cudaNormal
+	    
+	// TODO: Pointer auf die Daten von cudaPos[ping] und cudaPos[1-ping] beschaffen. (Hinweis: cudaGraphicsResourceGetMappedPointer)
+	void** posPing = NULL;
+	void** posPong = NULL;
+	size_t* posPingSize = NULL;
+	size_t* posPongSize = NULL;
+	CUDA_SAFE_CALL(cudaGraphicsResourceGetMappedPointer((void**)&newPos,posPingSize,cudaPos[ping]));
+	CUDA_SAFE_CALL(cudaGraphicsResourceGetMappedPointer((void**)&oldPos,posPongSize,cudaPos[1-ping]));
+	// TODO: Pointer auf die Daten von cudaNormal beschaffen.	
+
+	// Launch update
+	float stepSize = 0.5f; // steers how quickly the iterative refinement converges	
+	updateCloth((float3*)newPos, (float3*)oldPos, (float3*)devPtrImpact, (float3*)devPtrVelocity, deltaTime, stepSize);
+
+	// TODO: Unmap cudaNormal	
+	// TODO: Unmap cudaPos (Hinweis: cudaGraphicsUnmapResources)	
+	CUDA_SAFE_CALL(cudaGraphicsUnmapResources(2,cudaPos));
+
+	// Swap ping pong roles.
+	ping = 1-ping;
+}
+
+unsigned int ClothSim::getVBOPos(unsigned int p) const
+{
+	return vboPos[p];
+}
+
+unsigned int ClothSim::getVBONormal() const
+{
+	return vboNormal;
+}
+
+unsigned int ClothSim::getResolutionX() const
+{
+	return RESOLUTION_X;
+}
+
+unsigned int ClothSim::getResolutionY() const
+{
+	return RESOLUTION_Y;
+}
+
+unsigned int ClothSim::getPingStatus() const
+{
+	return ping;
+}
---
+++ sheet09_task_21_ClothSimulation/Cloth.cu
@@ -0,0 +1,137 @@
+
+// Includes
+#include "CudaMath.h"
+#include "Cloth.h"
+
+// Computes the impacts between two points that are connected by a constraint in order to satisfy the constraint a little better.
+__device__ float3 computeImpact(float3 me, float3 other, float stepsize, float h)
+{	
+	const float aimedDistance = 1.0 / (float)RESOLUTION_X;
+	float3 dir = other-me;
+	float ldir = length(dir);
+	if (ldir==0) return dir;
+	float e = (ldir - aimedDistance) * 0.5;
+	float3 debug = dir/ldir * e / (h*h) * stepsize; 
+	return debug;
+}
+
+// Simple collision detection against a sphere at (0,0,0) with radius SPHERE_RADIUS and skin width SKIN_WIDTH.
+__device__ float3 sphereCollision(float3 p, float h)
+{
+	// TODO: Testen, ob Punkt im inneren der Kugel ist. Wenn ja, dann einen Impuls berechnen, der sie wieder heraus bewegt.
+	return p;
+}
+
+// -----------------------------------------------------------------------------------------------
+// Aufsummieren der Impulse, die von den benachbarten Gitterpunkten ausgeübt werden.
+// impacts += ...
+__global__ void computeImpacts(float3* oldPos, float3* impacts, float stepsize, float h)
+{
+	// TODO: Positionen der benachbarten Gitterpunkte und des eigenen Gitterpunktes ablesen.
+	int myIndex = blockIdx.x * RESOLUTION_Y + blockIdx.y;
+	int up = blockIdx.y -1;
+	int down = blockIdx.y +1;
+	int left = blockIdx.x -1;
+	int right = blockIdx.x +1;
+
+	float3 impactsTmp = make_float3(0, 0, 0);
+
+	// TODO: Kollisionsbehandlung mit Kugel durchführen.
+
+	// TODO: Mit jedem Nachbar besteht ein Constraint. Dementsprechend für jeden Nachbar 
+	//		 computeImpact aufrufen und die Ergebnisse aufsummieren.
+	if(up > 0)
+	{
+		int upIdx = blockIdx.x * RESOLUTION_Y + up;
+		float3 tmp = computeImpact(oldPos[myIndex], oldPos[upIdx],stepsize,h);
+		impactsTmp = impactsTmp + tmp;
+	}
+	if(down < RESOLUTION_Y-1)
+	{
+		int downIdx = blockIdx.x * RESOLUTION_Y + down;
+		float3 tmp = computeImpact(oldPos[myIndex], oldPos[downIdx],stepsize,h);
+		impactsTmp = impactsTmp + tmp;
+	}
+	if(left > 0)
+	{
+		int leftIdx = left * RESOLUTION_Y + blockIdx.y;
+		float3 tmp = computeImpact(oldPos[myIndex], oldPos[leftIdx],stepsize,h);
+		impactsTmp = impactsTmp + tmp; 
+	}
+	if(right < RESOLUTION_X)
+	{
+		int rightIdx = right * RESOLUTION_Y + blockIdx.y;
+		float3 tmp = computeImpact(oldPos[myIndex], oldPos[rightIdx],stepsize,h);
+		impactsTmp = impactsTmp + tmp;
+	}
+
+	// TODO: Die Summe der Impulse auf "impacts" des eigenen Gitterpunkts addieren.	
+	impacts[myIndex] = impacts[myIndex] + impactsTmp;
+}
+
+// -----------------------------------------------------------------------------------------------
+// Preview-Step
+// newpos = oldpos + (velocity + impacts * h) * h
+__global__ void previewSteps(	float3* newPos, float3* oldPos, float3* impacts, float3* velocity,								
+								float h)
+{
+	int index = blockIdx.x * RESOLUTION_Y + blockIdx.y;
+	// TODO: Berechnen, wo wir wären, wenn wir eine Integration von der bisherigen Position 
+	//		 mit der bisherigen Geschwindigkeit und den neuen Impulsen durchführen.
+	newPos[index] = oldPos[index] + velocity[index] * h + impacts[index] * h*h;
+	
+	//newPos[index] = oldPos[index] - make_float3(0,0.002,0);
+}
+
+// -----------------------------------------------------------------------------------------------
+// Integrate velocity
+// velocity = velocity * LINEAR_DAMPING + (impacts - (0,GRAVITY,0)) * h 
+__global__ void integrateVelocity(	float3* impacts, float3* velocity, float h)
+{
+	int index = blockIdx.x * RESOLUTION_Y + blockIdx.y;
+	// TODO: Update velocity.
+	float3 debug = velocity[index] * LINEAR_DAMPING + (impacts[index] - make_float3(0,GRAVITY,0)) * h; 
+	velocity[index] = debug;
+}
+
+// -----------------------------------------------------------------------------------------------
+// Test-Funktion die nur dazu da ist, damit man etwas sieht, sobald die VBOs gemapped werden...
+__global__ void test( float3* newPos, float3* oldPos, float h)
+{
+	newPos[blockIdx.x] = oldPos[blockIdx.x] + make_float3(0, -h, 0);
+}
+
+// -----------------------------------------------------------------------------------------------
+void updateCloth(	float3* newPos, float3* oldPos, float3* impacts, float3* velocity,					
+					float h, float stepsize)
+{
+	// dont move the row resY
+	dim3 blocks(RESOLUTION_X, RESOLUTION_Y-1, 1);
+
+	// -----------------------------
+	// Clear impacts
+	cudaMemset(impacts, 0, RESOLUTION_X*RESOLUTION_Y*sizeof(float3));
+
+	// Updating constraints is an iterative process.
+	// The more iterations we apply, the stiffer the cloth become.
+	for (int i=0; i<10; ++i)
+	{
+		// -----------------------------
+		// TODO: previewSteps Kernel aufrufen (Vorhersagen, wo die Gitterpunkte mit den aktuellen Impulsen landen würden.)
+		// newpos = oldpos + (velocity + impacts * h) * h		
+		previewSteps<<<blocks,1>>>(newPos, oldPos,impacts,velocity,h);
+		// -----------------------------
+		// TODO: computeImpacts Kernel aufrufen (Die Impulse neu berechnen, sodass die Constraints besser eingehalten werden.)
+		// impacts += ...
+		computeImpacts<<<blocks,1>>>(newPos,impacts,stepsize,h);
+	}
+
+	// -----------------------------
+	// TODO: Approximieren der Normalen
+	
+	// -----------------------------
+	// TODO: Integrate velocity kernel ausführen
+	// Der kernel berechnet:  velocity = velocity * LINEAR_DAMPING + (impacts - (0,GRAVITY,0)) * h 	
+	integrateVelocity<<<blocks,1>>>(impacts,velocity,h);
+	//previewSteps<<<blocks,1>>>(newPos, oldPos,impacts,velocity,h);
+}
